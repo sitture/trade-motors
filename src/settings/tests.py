3895eb11c1c8b332dea7ad2d462874eeac053c2e
Haroon <haroon@sitture.com>
2015-07-13 02:38:45 +0100

    general formatting

--- src/settings/tests.py
+++ src/settings/tests.py
@@ -8,11 +8,20 @@ class ContactDetailModelTest(TestCase):
     def test_str_with_no_fullname(self):
         email_address = 'test@sitture.com'
         detail = ContactDetail(email=email_address)
-        self.assertEquals(str(detail), detail.email)
+        self.assertEquals(
+            str(detail),
+            detail.email
+        )
         
     def test_str_with_fullname(self):
         email_address = 'test@sitture.com'
         fullname = 'Sitture'
         expected_result = '{0} ({1})'.format(fullname, email_address)
-        detail = ContactDetail(full_name=fullname, email=email_address)
-        self.assertEquals(str(detail), expected_result)
+        detail = ContactDetail(
+            full_name=fullname,
+            email=email_address
+        )
+        self.assertEquals(
+            str(detail),
+            expected_result
+        )
