dc06ff0077f040af50981736e5a3ad4ce34ffd96
Haroon <haroon@sitture.com>
2015-07-13 02:38:03 +0100

    adding overall project tests

---
+++ src/gp_cars/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+class ProjectTests(TestCase):
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
