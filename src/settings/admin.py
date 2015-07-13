d544ed69212f1b1d7cef7613b19fe2e72a2efbdc
Haroon <haroon@sitture.com>
2015-07-13 02:49:59 +0100

    registering the contact details model

--- src/settings/admin.py
+++ src/settings/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from settings.models import ContactDetail
+
+# register the contact details model
+class ContactDetailAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        model = ContactDetail
+
 
 # Register your models here.
+admin.site.register(ContactDetail, ContactDetailAdmin)
