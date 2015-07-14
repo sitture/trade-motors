43bb7824ae72674b15cb122e38de2080381273d2
Haroon <haroon@sitture.com>
2015-07-14 14:57:45 +0100

    adding a custom context processor to create global variables

---
+++ src/vehicles/context_processor.py
@@ -0,0 +1,34 @@
+# Context processors
+from settings.models import Social
+from vehicles.models import Category
+
+# to allow these variables to available in all views.
+def global_context_processor(request):
+	# a 
+	social_providers = list(Social.objects.all())
+	# get all of the categories
+	category_objects = Category.objects
+	# filter categories without parents
+	categories = list(category_objects.filter(category_parent=None))
+	# categories with parent categories
+	sub_categories = list(category_objects.exclude(category_parent=None))
+
+	all_categories = get_all_categories(categories, sub_categories)
+
+	return locals()
+
+def get_all_categories(categories, sub_categories):
+
+	formatted_categories = []
+
+	for category in categories:
+		tmp = sub_temp = []
+		for sub_category in sub_categories:
+			if category == sub_category.category_parent:
+				sub_temp.append(sub_category)
+		tmp = [category, sub_temp]
+		formatted_categories.append(tmp)
+
+	return formatted_categories
+
+
--- src/vehicles/views.py
+++ src/vehicles/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render, render_to_response, RequestContext
+# import the custom context processor
+from vehicles.context_processor import global_context_processor
 
 from vehicles.models import Vehicle, Category
 
-def home_page(request):
-    
-    # get all of the categories
-    category_objects = Category.objects
-    # filter categories without parents
-    categories = list(category_objects.filter(category_parent=None))
-    # categories with parent categories
-    sub_categories = list(category_objects.exclude(category_parent=None))
-    
+def home_page(request):    
     return render_to_response("home_page.html",
                               locals(), # gives access to local variables
-                              context_instance=RequestContext(request))
+                              context_instance=RequestContext(request, processors=[global_context_processor]))
