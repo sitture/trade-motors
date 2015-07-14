85fdc7d1094b4229c9c85a00c2c6194fd6f737ae
Haroon <haroon@sitture.com>
2015-07-14 15:09:15 +0100

    adding tests for contact and about us pages

--- src/gp_cars/tests.py
+++ src/gp_cars/tests.py
@@ -5,3 +5,11 @@ class ProjectTests(TestCase):
     def test_homepage(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page(self):
+        response = self.client.get('/about-us')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_us_page(self):
+        response = self.client.get('/contact-us')
+        self.assertEqual(response.status_code, 200)
