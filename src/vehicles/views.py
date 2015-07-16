af458c98c7c8aa5790bf64d672fe66bbbbd070eb
Sheikh, Haroon <haroon.sheikh@bskyb.com>
2015-07-16 17:02:55 +0100

    removing print statements

--- src/vehicles/views.py
+++ src/vehicles/views.py
@@ -11,13 +11,12 @@ def home_page(request):
         						request, processors=[global_context_processor]))
 
 
-def category_page(request, category_slug):
+def category_page(request, slug):
 	# get category by slug
-	category = Category.objects.filter(slug=category_slug)
-	print category
+	category = Category.objects.filter(slug=slug)
 	# get all the vehicles by the category slug
 	vehicles_in_category = list(Vehicle.objects.filter(category=category))
-	print vehicles_in_category
+
 	return render_to_response("home_page.html", locals(),
                               context_instance=RequestContext(
         						request, processors=[global_context_processor]))
