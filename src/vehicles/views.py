7561a1fcdcaa9f9c5aa8dabfa50e4133c58c3118
Haroon <haroon@sitture.com>
2015-07-20 01:37:12 +0100

    pagination added to vehicle list

--- src/vehicles/views.py
+++ src/vehicles/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, render_to_response, RequestContext
 from vehicles.context_processor import global_context_processor
 
 from vehicles.models import Vehicle, VehicleMake, Category
+from django.core.paginator import Paginator
 
 
 def home_page(request):
@@ -25,6 +26,17 @@ def category_page(request, slug):
             category, make)
     else:
         vehicles_list = Vehicle.objects.get_vehicles_by_category(category)
+    
+    # paginate vehicle list for 10 items per page
+    paginator = Paginator(vehicles_list, 10)
+    
+    try: page = int(request.GET.get("page", '1'))
+    except ValueError: page = 1
+    
+    try:
+        vehicles = paginator.page(page)
+    except (InvalidPage, EmptyPage):
+        vehicles = paginator.page(paginator.num_pages)
         
     return render_to_response("home_page.html", locals(),
         context_instance=RequestContext(request, processors=[global_context_processor]))
