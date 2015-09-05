dfd0d962cd46c179dc942ac62f7f17199abeb002
Haroon <haroon@sitture.com>
2015-09-05 22:21:49 +0100

    context processor tests segregated

--- src/vehicles/tests/test_models.py
+++ src/vehicles/tests/test_models.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 # import the models to test
 from vehicles.models import Category, Vehicle, VehicleMake
-from vehicles import context_processor
 from vehicles.views import get_makes_in_category
 
 
@@ -269,31 +268,6 @@ class VehicleQuerySetTest(TestCase):
         self.assertEquals(
             [],
             list(actual_vehicles)
-        )
-
-
-class ContextProcessorTest(TestCase):
-
-    def test_get_all_categories_in_a_list(self):
-        # setup data for test
-        main_category = Category.objects.create(
-            category_name='Main Category',
-            slug='main-category'
-        )
-        sub_category = Category.objects.create(
-            category_parent=main_category,
-            category_name='Sub Category',
-            slug='sub-category'
-        )
-        expected_result = [[main_category, [sub_category]]]
-        # get the actual data
-        categories = list(Category.objects.filter(category_parent=None))
-        sub_categories = list(Category.objects.exclude(category_parent=None))
-        actual_result = context_processor.get_all_categories(
-            categories, sub_categories)
-
-        self.assertEquals(
-            expected_result, actual_result
         )
 
 
