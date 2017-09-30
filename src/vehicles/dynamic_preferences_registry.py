89e6aeb60e80f76b9558ab67ebaacebe7b402f3b
Parth Verma <v.parth98@gmail.com>
2017-09-30 14:46:00 +0530

    Upgraded to django version 1.11.5

--- src/vehicles/dynamic_preferences_registry.py
+++ src/vehicles/dynamic_preferences_registry.py
@@ -1,6 +1,6 @@
 from dynamic_preferences.types import StringPreference, \
     IntegerPreference, BooleanPreference
-from dynamic_preferences import global_preferences_registry
+from dynamic_preferences.registries import global_preferences_registry
 from django.conf import settings
 
 
